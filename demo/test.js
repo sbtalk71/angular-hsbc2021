65af910cfac6550b2f36cafe87c25161797d7a07
shan71 <sbtalk@hotmail.com>
2021-09-30 22:32:36 +0530

    angular apps added

---
+++ demo/test.js
@@ -0,0 +1,13 @@
+demo=function(){ console.log("Hello there!")}
+
+demo()
+
+
+demo1=()=> console.log("Hello there!")
+
+demo1()
+
+//python y=lambda x:x+x
+//java  y = (x)->x+x
+
+//Reactive Programing
